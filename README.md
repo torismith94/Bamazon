9f50dc47dac8ac1a0ece8716287c96aebfa71976
Victoria Smith <torismith@Victorias-MacBook-Pro-3.local>
2017-11-17 19:42:47 -0800

    final changes, updated ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Bamazon
+
+##Overview
+
+The app will take in orders from customers and deplete stock from the store's inventory. When you run the CLI app, a list of all available products will appear. You will then be prompted with what product you would like to buy and the quantity. 
+
+![List View of Items](/images/list_of_products.png)
+
+If stock is available, you will be show your order total. If the stock is unavailable, your order will not be completed. 
+
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,77 +17,85 @@ connection.connect(function(err) {
 });
 
 function start() {
-	connection.query("SELECT * FROM products", function (err, res) {
-		for (var i = 0; i < res.length; i++) {
-			console.log(res[i].item_id + " | " + res[i].product_name + " | "+ "$" + res[i].price);
+	connection.query("SELECT * FROM products", function (err, results) {
+		for (var i = 0; i < results.length; i++) {
+			console.log(results[i].item_id + " | " + results[i].product_name + " | "+ "$" + results[i].price);
 		}
-		order();
+		order(results);
 
 	});
 }
 
 
-function order() {
+function order(query) {
 	inquirer
-	.prompt({
-		name: "product_id",
+	.prompt([{
+		name: "item_id",
 		type: "input", 
 		message: "What is the ID of the product you would like to buy?"
-	})
-	.then(function(answer) {
-		if (answer.product_id === "1", "2", "3", "4", "5", "6", "7", "8", "9", "10") {
-			console.log("You bought item " + answer.product_id);
-		}
-		buyItem();
-	});
-}
-
-
-function buyItem() {
-	inquirer
-	.prompt({
+	},
+	{
 		name: "quantity",
 		type: "input",
 		message: "How many units of the item would you like to buy?"
-	})
-	.then(function(answer){
-		var quantityBought;
-		for (var i = 0; i < results.length; i++) {
-		  if (results[i].stock_quantity === answer.choice) {
-			quantityBought = results[i];
-		  }
+	}])
+	.then(function(results) {
+		var validItem = false;
+		for (var i = 0; i < query.length && !validItem; i++) {
+		  if (query[i].item_id === parseInt(results.item_id)) {
+				validItem = true;
+		 	}
+		 }
+		if (validItem) {
+			checkQuantity(parseInt(results.item_id), parseInt(results.quantity));
 		}
-
-		if (quantityBought.stockquantity < parseInt(answer.bid)){
-			
-		}
-	}
-
-		)
-
-
-
-
-
-	.then(function(answer){
-		connection.query(
-			"INSERT INTO bamazon SET",
-			{
-				stock_quantity: answer.stockquantity
-
-			},
-			function(err) {
-				if (err) throw err;
-				console.log("There is not enough stock to fulfill your order. Your order was not placed.");
-			}
-			);
-		console.log('You bought ' + answer.quantity + ' units.');
-	
-});
+		else {
+			console.log("Not a valid item. Please choose one from the list above.")
+			connection.end();
+		}	
+		// if (answer.product_id === "1", "2", "3", "4", "5", "6", "7", "8", "9", "10") {
+		// 	console.log("You bought item " + answer.product_id);
+		// }
+		// buyItem();
+	});
 }
 
 
-	
+function checkQuantity (id, quantity) {
+	connection.query("SELECT * from products where?", 
+		[{
+			item_id: id
+		}], 
+		function(err, results) {
+			if (err) throw err; 
+			else {
+				if(results[0].stock_quantity >= parseInt(quantity)) {
+					var total = parseInt(quantity) * results[0].price;
+					console.log("Your total is $" + total)
+					updateDb(id, quantity, results[0].stock_quantity);
+				}
+				else {
+					console.log("There is not enough stock to fulfill your order. Your order was not placed.");
+					connection.end();
+				}
+			}
+			})
+	}
 
+function updateDb(id, requested, instock) {
+	connection.query("UPDATE products SET ? WHERE ?", 
+		[
+			{
+				stock_quantity: (instock-requested)
+			},
+			{
+				item_id: id
+			}
+		],
+		function(err,res){
+			console.log("Complete!");
+			connection.end();
+		})
+}
 
 
